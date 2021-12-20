89549f203171f6e673083a34ce8eab057dfa8ece
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-12-20 10:43:25 -0300

    Criando projetos

---
+++ Swift/Challanges/Challanges/BoyOrGirl.swift
@@ -0,0 +1,16 @@
+//
+//  BoyOrGirl.swift
+//  Challanges
+//
+//  Created by Carlos Amaral on 20/12/21.
+//  https://codeforces.com/problemset/problem/236/A
+//
+
+import Foundation
+
+func BoyOrGirl() {
+    //NÚMERO DE CARACTERES DISTINTOS É PAR?
+    // SIM - GIRL
+    // NAO - MAN
+    
+}
---
+++ Swift/Challanges/Challanges/WordCapitalization.swift
@@ -0,0 +1,14 @@
+//
+//  WordCapitalization.swift
+//  Challanges
+//
+//  Created by Carlos Amaral on 20/12/21.
+//  https://codeforces.com/problemset/problem/281/A
+//
+
+import Foundation
+
+func WordCapitalization() {
+    if let w = readLine() { print("\(w.capitalized)") }
+    else { return }
+}
